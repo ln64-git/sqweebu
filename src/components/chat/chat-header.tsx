88db2b9e73fcf08487cdaeb919872875aa37dfe3
Lucian <lucian.io@outlook.com>
2024-02-21 00:22:54 -0500

    added playback controls

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,13 +1,31 @@
+import { Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Image from "next/image";
+import PlaybackControls from "../playback-controls";
+
 export default function Header() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <div className="fixed top-0 w-full bg-zinc-950 bg-opacity-60 z-10 flex justify-center items-center backdrop-filter backdrop-blur-xl ">
-      <div className="bg-zinc-950 bg-opacity-80 rounded-md m-1 backdrop-blur-sm">
-        <input
-          type="text"
-          className="w-full caret-transparent rounded-md px-4 text-xs bg-transparent text-zinc-500 placeholder-zinc-400 text-center backdrop-blur-lg placeholder-opacity-50 outline-none focus:outline-none" // Removed selected effects and made the font smaller
-          placeholder="Command"
+    <div className="fixed top-0 w-full bg-zinc-950 bg-opacity-60 z-10 flex justify-between items-center backdrop-filter backdrop-blur-xl px-4">
+      <div className="flex items-center">
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          aria-label="Toggle navigation"
+          size={"sm"}
+          color="dark"
+          className="mr-4" // Add margin to separate from input
         />
+        <div className="bg-zinc-950 bg-opacity-80 rounded-md m-1 backdrop-blur-sm">
+          <input
+            type="text"
+            className="w-full caret-transparent py-2 rounded-md px-4 text-xs bg-transparent text-zinc-500 placeholder-zinc-400 text-center backdrop-blur-lg placeholder-opacity-50 outline-none focus:outline-none" // Removed selected effects and made the font smaller
+            placeholder="Command"
+          />
+        </div>
       </div>
+      <PlaybackControls />
     </div>
   );
 }
