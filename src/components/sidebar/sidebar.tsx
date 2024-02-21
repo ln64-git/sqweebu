b511f90b1bbd05d8e5d91821c2f1934bbde8722e
Lucian <lucian.io@outlook.com>
2024-02-21 06:00:01 -0500

    added a chat history section

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -5,12 +5,14 @@ import keyboard from "../../../public/sidebar/keyboard.svg";
 import command from "../../../public/sidebar/command.svg";
 import person from "../../../public/sidebar/person.svg";
 import model from "../../../public/sidebar/model.svg";
+import chat from "../../../public/sidebar/chat.svg";
 import HoverableIcon from "@/utils/hoverable-icon";
 
 export default function SideBar() {
   return (
     <div className="flex flex-col justify-between h-full">
       <div className="flex flex-col items-center gap-2 pt-3">
+        <HoverableIcon src={chat} alt="model" />
         <HoverableIcon src={model} alt="model" />
         <HoverableIcon src={person} alt="person" />
         <HoverableIcon src={command} alt="command" />
