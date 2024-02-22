daa8fb6cf780aa78f87e69f40f959a2eec6ad406
Lucian <lucian.io@outlook.com>
2024-02-22 06:03:23 -0500

    rearranged sidebar icons, added plus button

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -10,7 +10,7 @@ import command from "../../../public/sidebar/command.svg";
 import voice from "../../../public/sidebar/voice.svg";
 import model from "../../../public/sidebar/model.svg";
 import chat from "../../../public/sidebar/chat.svg";
-import home from "../../../public/sidebar/home.svg";
+import add from "../../../public/sidebar/add.svg";
 
 export default function SideBar() {
   const sidebar = useNexus((state) => state.sidebar);
@@ -23,15 +23,20 @@ export default function SideBar() {
           style={{ backgroundColor: overlayColor }} // Corrected usage of overlayColor
           className="md:block min-w-[61px] opacity-60 flex flex-col justify-between"
         >
-          <div className="flex flex-col items-center gap-3 pt-3.5">
-            <HoverableIcon src={chat} alt="chat" />
-            <HoverableIcon src={model} alt="model" />
-            <HoverableIcon src={voice} alt="voice" />
-            <HoverableIcon src={command} alt="command" />
-            <HoverableIcon src={keyboard} alt="keyboard" />
-            <HoverableIcon src={settings} alt="settings" />
+          <div className="flex flex-col justify-between items-center  pt-2.5  h-full">
+            <div className="flex flex-col justify-center items-center gap-3  w-full h-[134px] ">
+              <HoverableIcon src={add} alt="add" />
+              <HoverableIcon src={chat} alt="chat" />
+              <HoverableIcon src={model} alt="model" />
+              <HoverableIcon src={voice} alt="voice" />
+            </div>
+            <div className="flex flex-col justify-between items-center gap-3 w-full  pb-2">
+              <HoverableIcon src={command} alt="command" />
+              <HoverableIcon src={keyboard} alt="keyboard" />
+              <HoverableIcon src={settings} alt="settings" />
+            </div>
           </div>
-          <div className="pt-4">
+          <div className="pt-[2px]">
             <LightSwitch />
           </div>
         </div>
